a2622dd250598c75e42eeb1066f067cc9e8d3412
Massimiliano Romana <m.romana@simularia.it>
2023-06-27 17:14:05 +0200

    Added factor.py code.

---
+++ factor.py
@@ -0,0 +1,17 @@
+
+
+
+
+def computefactor(scheme, ws, height):
+
+
+    match scheme:
+        case 1:
+            # odour
+            # inserire formula per l'odore
+        case 2:
+            # cumuli alti?
+        case 3:
+            # cumuli bassi?
+
+    return
