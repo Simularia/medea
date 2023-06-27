496566f40d366ec8a58003fdb62261dd065b49de
Massimiliano Romana <m.romana@simularia.it>
2023-06-27 17:07:49 +0200

    Updated met.py module and added windinputfile.csv example.

--- met.py
+++ met.py
@@ -7,12 +7,20 @@ from factor import factor
 
 
 def readmet(conf):
+    # rea input csv meteo file
     met = pd.read_csv(conf['windinputfile'])
+    return met
+
+
+def writemet(conf, met):
+    # build output meteo and factor dataframe
     columns = ['date', 'wd', 'ws']
     for i in conf['source']:
         for j in conf['species']:
             columns.append(str(i)+j)
     metout = pd.DataFrame([],columns=columns, index = range(1, len(met.index + 1)))
+
+    # loop on all the deadlines
     for idx in met.index:
         ws = met.iloc[idx, 'ws']
         date = met.iloc[idx, 'date']
@@ -27,5 +35,6 @@ def readmet(conf):
         metout.iloc[idx, 'date'] = date
         metout.iloc[idx, 'ws'] = ws
         metout.iloc[idx, 'wd'] = wd
+    # write the output csv meteo and factor file
     metout.to_csv(conf['windoutputfile'])
     return metout
